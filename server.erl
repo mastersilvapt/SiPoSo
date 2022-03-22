f47071e8561a0d700b7e1cce61b2e9c5143a1978
mastersilvapt <andresilva.aer@gmail.com>
2022-03-22 23:08:21 +0000

    Refactoring code

--- server.erl
+++ server.erl
@@ -25,9 +25,9 @@ replay(From, Ref, Response) ->
 
 %normalize({VS,C,ES}) ->
 
-handler({sum,XS,YS}) -> sum(XS,YS);
-handler({sub,XS,YS}) -> sub(XS,YS);
-handler({mult,XS,YS}) -> mult(XS,YS);
+handler({sum,{XS,YS}}) -> sum(XS,YS);
+handler({sub,{XS,YS}}) -> sub(XS,YS);
+handler({mult,{XS,YS}}) -> mult(XS,YS);
 handler(_) -> ok.
 
 %head([X|_]) -> X.
@@ -38,7 +38,8 @@ getSum([{_,C,_}|XS]) -> C + getSum(XS).
 filter(t,V,E,XS) -> lists:filter(fun({Vf,_,Ef}) -> (V == Vf) and (E == Ef) end,XS);
 filter(f,V,E,XS) -> lists:filter(fun({Vf,_,Ef}) -> (V /= Vf) or (E /= Ef) end,XS).
 
-clean(XS) -> [{V,C,E} || {V,C,E} <- XS, C /= 0].
+% clean(XS) -> [{V,C,E} || {V,C,E} <- XS, C /= 0].
+clean(XS) -> lists:filter(fun({V,C,E}) -> C /= 0 end, XS).
 
 normalize([]) -> [];
 normalize([{V,C,E}|XS]) -> [{V, C+getSum(filter(t,V,E,XS)), E}] ++ normalize(filter(f,V,E,XS)).
@@ -48,16 +49,15 @@ sum(XS,YS) -> normalize(lists:append(XS,YS)).
 negate([]) -> [];
 negate([{V,C,E}|XS]) -> [{V,-C,E}] ++ negate(XS).
 
-
 sub(XS,YS) -> sum(XS,negate(YS)).
 
-
-recNub([],_) -> [];
-recNub([R|XS],R) -> recNub(XS,R);
-recNub([X|XS],R) -> [X] ++ recNub(XS,R).
+%recNub([],_) -> [];
+%recNub([R|XS],R) -> recNub(XS,R);
+%recNub([X|XS],R) -> [X] ++ recNub(XS,R).
 
 nub([]) -> [];
-nub([X|XS]) -> [X] ++ nub(recNub(XS,X)).
+nub([X|XS]) -> [X] ++ nub(lists:filter(fun(Xi) -> Xi /= X end, XS)).
+%nub([X|XS]) -> [X] ++ nub(recNub(XS,X)).
 
 calculateVars(V1,V2) -> nub(lists:append(V1,V2)).
 
@@ -73,3 +73,4 @@ second([{_,S}|XS]) -> [S] ++ second(XS).
 calculateExp(V1,V2,E1,E2) -> second(join(lists:sort(fun({V10,_},{V11,_}) -> V10 < V11 end, lists:zip(V1,E1)), lists:sort(fun({V20,_},{V21,_}) -> V20 < V21 end, lists:zip(V2,E2)))).
 
 mult(XS,YS) -> normalize([{calculateVars(V1,V2), C1*C2, calculateExp(V1,V2,E1,E2)} || {V1,C1,E1} <- XS, {V2,C2,E2} <- YS]).
+%mult(XS,YS) -> normalize([ lists:append([ || {Vi1,Ei1} <- lists:sort(fun({V10,_},) -> end, lists:zip(V1,E1)), {Vi2,Ei2} <- lists:zip(V2,E2)]) || {V1,C1,E1} <- XS, {V2,C2,E2} <- YS]).
