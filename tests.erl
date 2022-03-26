44f8c527ba52a2368df8cf5c01dc0b28e6cc41d5
mastersilvapt <andresilva.aer@gmail.com>
2022-03-26 02:01:04 +0000

    renamed files

--- tests.erl
+++
@@ -1,9 +0,0 @@
--module(tests).
--export([normalize/1]).
-
-getSum([]) -> 0;
-getSum([{_,C,_}|XS]) -> C + getSum(XS).
-
-
-normalize([]) -> [];
-normalize([{V,C,E}|XS]) -> [{V,C + getSum(lists:filter(fun({Vf,_,Ef}) -> (V == Vf) and (E == Ef) end,XS)),E}] ++ normalize(lists:filter(fun({Vf,_,Ef}) -> (V /= Vf) or (E /= Ef) end,XS)).
